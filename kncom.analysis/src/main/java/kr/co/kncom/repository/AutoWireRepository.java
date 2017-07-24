59830cd9c1fc656442526798fedea801463fd5e9
kndoll <kndoll@DESKTOP-23DSFMS>
2017-07-24 11:16:01 +0900

    테스트 파일 삭제

--- kncom.analysis/src/main/java/kr/co/kncom/repository/AutoWireRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.co.kncom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kr.co.kncom.domain.AuctionList;
-
-public interface AutoWireRepository  extends JpaRepository<AuctionList, String> {
-
-}
