84ed718c1207f4a007dd6f56f8ab9b4a0f6be32d
kndoll <kndoll@DESKTOP-23DSFMS>
2017-07-31 16:44:07 +0900

    리펙토링

--- kncom.analysis/src/main/java/kr/co/kncom/util/FileUtil.java
+++ kncom.analysis/src/main/java/kr/co/kncom/util/FileUtil.java
@@ -52,7 +52,6 @@ public class FileUtil {
 				rtnList.add(strLine.split("\\" + separator));
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
